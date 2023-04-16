9cf3f704a98cf90e5d8b94c7848a185694cfd68f
Michael <michael.oneill.2020@mumail.ie>
2023-04-16 13:29:50 +0100

    Implement TryFind methods for all remaining hand classes

--- src/PokerLogic/HandDeterminer.cs
+++ src/PokerLogic/HandDeterminer.cs
@@ -7,19 +7,52 @@ public static class HandDeterminer
 {
     public static Hand GetHand(Card[] cards)
     {
+        HandClass handClass;
+
         SortDescending(cards);
 
-        if (TryFindTwoPair(cards))
+        if (TryFindRoyalFlush(cards))
         {
-            return new Hand() { Class = HandClass.TwoPair, CompareOrder = cards };
+            handClass = HandClass.RoyalFlush;
+        }
+        else if (TryFindStraightFlush(cards))
+        {
+            handClass = HandClass.StraightFlush;
+        }
+        else if (TryFindFourOfAKind(cards))
+        {
+            handClass = HandClass.FourOfAKind;
+        }
+        else if (TryFindFullHouse(cards))
+        {
+            handClass = HandClass.FullHouse;
+        }
+        else if (TryFindFlush(cards))
+        {
+            handClass = HandClass.Flush;
+        }
+        else if (TryFindStraight(cards, true))
+        {
+            handClass = HandClass.Straight;
+        }
+        else if (TryFindThreeOfAKind(cards))
+        {
+            handClass = HandClass.ThreeOfAKind;
+        }
+        else if (TryFindTwoPair(cards))
+        {
+            handClass = HandClass.TwoPair;
+        }
+        else if (TryFindPair(cards))
+        {
+            handClass = HandClass.Pair;
+        }
+        else
+        {
+            handClass = HandClass.HighCard;
         }
 
-        if (TryFindPair(cards))
-        {
-            return new Hand() { Class = HandClass.Pair, CompareOrder = cards };
-        }
-
-        return new Hand() { Class = HandClass.HighCard, CompareOrder = cards };
+        return new Hand() { Class = handClass, CompareOrder = cards };
     }
 
     private static bool TryFindPair(Card[] cards)
@@ -67,10 +100,102 @@ public static class HandDeterminer
         return TryFindDuplicates(cards, 3);
     }
 
+    private static bool TryFindFlush(Card[] cards)
+    {
+        for (var i = 1; i < cards.Length; i++)
+        {
+            if (cards[i].Suit != cards[i - 1].Suit)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool TryFindStraight(Card[] cards, bool sortInCompareOrder)
+    {
+        // Check Five-High Straight:
+        if (cards[0].Rank == Rank.Ace
+            && cards[1].Rank == Rank.Five
+            && cards[2].Rank == Rank.Four
+            && cards[3].Rank == Rank.Three
+            && cards[4].Rank == Rank.Two)
+        {
+            if(sortInCompareOrder)
+            {
+                Array.Sort(cards, (c1, c2) =>
+                {
+                    if (c1.Rank == Rank.Ace) return 1;
+                    if (c2.Rank == Rank.Ace) return -1;
+                    return -c1.CompareTo(c2);
+                });
+            }
+            return true;
+        }
+
+        for (var i = 1; i < cards.Length; i++)
+        {
+            if (cards[i].Rank - cards[i - 1].Rank != -1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool TryFindFullHouse(Card[] cards)
+    {
+        if (AreEqualInRank(cards[0], cards[1], cards[2])
+            && !AreEqualInRank(cards[2], cards[3])
+            && AreEqualInRank(cards[3], cards[4]))
+        {
+            var tripletRank = cards[0].Rank;
+            Array.Sort(cards, (c1, c2) =>
+            {
+                if (c1.Rank == c2.Rank) return -c1.CompareTo(c2);
+                if (c1.Rank == tripletRank) return -1;
+                if (c2.Rank == tripletRank) return 1;
+                return -c1.CompareTo(c2);
+            });
+            return true;
+        }
+
+        if (AreEqualInRank(cards[4], cards[3], cards[2])
+            && !AreEqualInRank(cards[2], cards[1])
+            && AreEqualInRank(cards[1], cards[0]))
+        {
+            var tripletRank = cards[4].Rank;
+            Array.Sort(cards, (c1, c2) =>
+            {
+                if (c1.Rank == c2.Rank) return -c1.CompareTo(c2);
+                if (c1.Rank == tripletRank) return -1;
+                if (c2.Rank == tripletRank) return 1;
+                return -c1.CompareTo(c2);
+            });
+            return true;
+        }
+
+        return false;
+    }
 
     private static bool TryFindFourOfAKind(Card[] cards)
     {
         return TryFindDuplicates(cards, 4);
+    }
+
+    private static bool TryFindStraightFlush(Card[] cards)
+    {
+        return TryFindFlush(cards) && TryFindStraight(cards, true);
+    }
+
+    private static bool TryFindRoyalFlush(Card[] cards)
+    {
+        return (TryFindFlush(cards)
+            && cards[0].Rank == Rank.Ace
+            && cards[1].Rank == Rank.King
+            && cards[2].Rank == Rank.Queen
+            && cards[3].Rank == Rank.Jack
+            && cards[4].Rank == Rank.Ten);
     }
 
     private static bool TryFindDuplicates(Card[] cards, int numberOfDupes)
@@ -93,6 +218,15 @@ public static class HandDeterminer
         }
 
         return false;
+    }
+
+    private static bool AreEqualInRank(params Card[] cards)
+    {
+        for (var i = 1; i < cards.Length; i++)
+        {
+            if (cards[i].Rank != cards[i - 1].Rank) return false;
+        }
+        return true;
     }
 
     private static void SortDescending(Card[] cards)
