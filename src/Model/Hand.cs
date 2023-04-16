f470094bb4bd9eb46f5d6a3b4bda468ae10f5102
Michael <michael.oneill.2020@mumail.ie>
2023-04-16 15:46:59 +0100

    Make Hand IComparable and implement CompareTo

--- src/Model/Hand.cs
+++ src/Model/Hand.cs
@@ -1,7 +1,32 @@
 ﻿namespace Model;
 
-public class Hand
+public class Hand : IComparable<Hand>
 {
     public HandClass Class { get; set; }
     public Card[] CompareOrder { get; set; }
+
+    public int CompareTo(Hand? other)
+    {
+        if(other == null)
+        {
+            throw new ArgumentNullException("other");
+        }
+
+        if(this.Class == other.Class)
+        {
+            for(var i = 0; i < CompareOrder.Length; i++)
+            {
+                var thisCard = this.CompareOrder[i];
+                var otherCard = other.CompareOrder[i];
+
+                int cardComparison = thisCard.CompareTo(otherCard);
+
+                if (cardComparison == 0) continue;
+
+                return cardComparison;
+            }
+        }
+
+        return this.Class.CompareTo(other.Class);
+    }
 }
